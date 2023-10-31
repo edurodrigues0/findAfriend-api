55158a5b5e7edc8f1d3fc115bf8fba99fe22b932
edurodrigues0 <edusacramento9@gmail.com>
2023-10-31 19:56:13 -0300

    feat: Initial project structure

---
+++ src/env/index.ts
@@ -0,0 +1,20 @@
+import 'dotenv/config'
+import { z } from 'zod'
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+  PORT: z.coerce.number().default(3333),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+// validacao da constante _env,
+// caso o valor seja igual a false,
+// derruba a aplicao com o throw new Error
+if (_env.success === false) {
+  console.error('❌ Invalid environment variables', _env.error.format())
+
+  throw new Error('Invalid environment variables.')
+}
+
+export const env = _env.data
